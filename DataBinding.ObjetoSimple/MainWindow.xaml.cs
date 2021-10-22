cd3cd714513994c0dcbcae22990efce0a699636f
Jose Eduardo Gongora Can <18470231@campeche.tecnm.mx>
2021-10-21 21:08:37 -0500

    Sincroniza el modelo de datos y la interfaz de usuario utilizando Data Binding a un objeto simple.

---
+++ DataBinding.ObjetoSimple/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows;
+using DataBinding.ObjetoSimple.Model;
+
+namespace DataBinding.ObjetoSimple
+{
+   
+    public partial class MainWindow : Window
+    {
+        private Eventos evento;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            evento = new Eventos()
+            {
+                Title = "Reunion de academia",
+                Date = new DateTime(2021, 10, 22)
+            };
+            UpdateUI();
+        }
+
+        private void UpdateUI()
+        {
+            eventoTextBox.Text = evento.Title;
+            fechaDatePicker.SelectedDate = evento.Date;
+        }
+
+        private void modificarEventoButton_Click(object sender, RoutedEventArgs e)
+        {
+            evento.Title = evento.Title.ToUpper();
+            evento.Date = evento.Date.AddDays(1);
+            UpdateUI();
+        }
+    }
+}
---
+++ DataBinding.ObjetoSimple/Model/Eventos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DataBinding.ObjetoSimple.Model
+{
+    public class Eventos
+    {
+        private string _Title;
+        private DateTime _Date;
+
+        public string Title
+        {
+            get => _Title;
+            set
+            {
+                if(_Title != value)
+                {
+                    _Title = value;
+                }
+            }
+        }
+        public DateTime Date
+        {
+            get => _Date;
+            set
+            {
+                if(_Date != value)
+                {
+                    _Date = value;
+                }
+            }
+        }
+    }
+}
