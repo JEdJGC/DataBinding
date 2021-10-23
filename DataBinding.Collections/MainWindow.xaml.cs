8e99607e68c18387c9e88e12d80ff18c8f532925
Jose Eduardo Gongora Can <18470231@campeche.tecnm.mx>
2021-10-22 22:50:08 -0500

    Sincroniza la coleccion de datos con el TextBox, utilizando solo manejo de eventos.

---
+++ DataBinding.Collections/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using DataBinding.Collections.Model;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DataBinding.Collections
+{
+
+    public partial class MainWindow : Window
+    {
+
+        private List<User> users;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadUsers();
+            usersListBox.ItemsSource = users;
+        }
+        private void LoadUsers()
+        {
+            users = new List<User>();
+            users.Add(new User { Name = "Peter Parker" });
+            users.Add(new User { Name = "Tony Stark" });
+            users.Add(new User { Name = "Natasha Romanoff" });
+
+        }
+        private void addUserButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(!string.IsNullOrEmpty(userTextBox.Text))
+            {
+                User user = new User { Name = userTextBox.Text };
+                users.Add(user);
+                usersListBox.SelectedItem = user;
+                UpdateView();
+            }
+
+        }
+        private void changeUserButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(usersListBox.SelectedItem != null)
+            {
+                User user = usersListBox.SelectedItem as User;
+                user.Name = userTextBox.Text;
+                usersListBox.SelectedItem = user;
+                UpdateView();
+
+            }
+
+        }
+        private void deleteUserButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(usersListBox.SelectedItem != null)
+            {
+                User user = usersListBox.SelectedItem as User;
+                users.Remove(user);
+                UpdateView();
+            }
+        }
+
+        private void UpdateView()
+        {
+            userTextBox.Text = string.Empty;
+            usersListBox.Items.Refresh();
+            if (users.Count > 0)
+            {
+                deleteUserButton.IsEnabled = true;
+                changeUserButton.IsEnabled = true;
+            }
+            else
+            {
+                deleteUserButton.IsEnabled = false;
+                changeUserButton.IsEnabled = false;
+                usersListBox.SelectedIndex = -1;
+            }
+        }
+
+        private void usersListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(usersListBox.SelectedItem != null)
+            {
+                userTextBox.Text = (usersListBox.SelectedItem as User).Name;
+            }
+        }
+    }
+}
---
+++ DataBinding.Collections/Model/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBinding.Collections.Model
+{ 
+    public class User
+    {
+        private string _Name;
+
+        public string Name
+        {
+            get => _Name;
+            set
+            {
+                if(_Name != value)
+                {
+                    _Name = value;
+                }
+            }
+        }
+    }
+}
+    
+
+        
+        
+      
+    
+
